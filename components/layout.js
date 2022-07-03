4e79146b475a4aa73b4eadaa7811c7677c7ed317
eozef <eozefow@gmail.com>
2022-07-03 15:39:54 +1000

    bg color testing

--- components/layout.js
+++ components/layout.js
@@ -3,16 +3,16 @@ import Link from "next/link";
 export default function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="bg-fuchsia-100 mb-8 py-4">
+      <header className="bg-gray-600 mb-8 py-4">
         <div className="container mx-auto flex justify-center">
           <Link href="/">
             <a>🏡</a>
           </Link>
-          <span className="mx-auto">Welcome to my blog</span>{" "}
+          <span className="mx-auto">Welcome to My Blog</span>{" "}
         </div>
       </header>
       <main className="container mx-auto flex-1">{children}</main>
-      <footer className=" mt-8 py-4">
+      <footer className="bg-gray-600 mt-8 py-4">
         <div className="container mx-auto flex justify-center">
           &copy; 2022 Eozef
         </div>
